5263eb8063c129d9a3a16c992b3e10a148b52d3d
Niroshan Sugirtharatnam <codeniro@gmail.com>
2020-08-21 05:30:55 -0400

    show unavailable booking times as unavailable

--- views/AppointmentView.py
+++ views/AppointmentView.py
@@ -111,19 +111,11 @@ class AppointmentView(IViewable):
         self.month_value.set(today.strftime("%B"))
         self.txt_year.insert(0, today.strftime("%Y"))
 
-        now = datetime.now()
-        timestamp = datetime.timestamp(now)
-        print(timestamp)
-
-
         # setup all button functionalities
         self.__bind_event_handlers()
 
         # at the end, focus on the main user input
         # self.txt_username.focus()
-
-        # build schedule only search button is pressed. showing now only to aid in building ui
-        # self.__build_schedule()
 
         self.__build_doctors_list()
 
@@ -140,7 +132,7 @@ class AppointmentView(IViewable):
         self.btn_search_apmt.bind("<ButtonRelease-1>", self.__handle_search_available_bookings)
 
     def __handle_search_available_bookings(self, _):
-        self.search_available_bookings()
+        self.__build_schedule(self.search_available_bookings())
 
     def search_available_bookings(self):
         time_string = self.month_value.get() + " " + self.txt_day.get().zfill(2) + " " + self.txt_year.get()
@@ -153,13 +145,12 @@ class AppointmentView(IViewable):
 
         starting_timestring = time_string + " 10:00:00 +0400"
         ending_timestring = time_string + " 15:00:00 +0400"
-        start_timestamp = mktime(datetime.strptime(starting_timestring, format + " %H:%M:%S %z").timetuple())
-        ending_timestamp = mktime(datetime.strptime(ending_timestring, format + " %H:%M:%S %z").timetuple())
+        start_timestamp = mktime(datetime.strptime(starting_timestring, time_format + " %H:%M:%S %z").timetuple())
+        ending_timestamp = mktime(datetime.strptime(ending_timestring, time_format + " %H:%M:%S %z").timetuple())
         doctor_id = self.available_doctors[list(self.available_doctors.keys())[self.cbo_doctors.current()]]['id']
         unavailable_slots = Database.get_unavailable_timeslots_for_doctor(doctor_id, start_timestamp=start_timestamp,
                                                                           end_timestamp=ending_timestamp)
-        print(unavailable_slots)
-        self.__build_schedule()
+        return unavailable_slots
 
     def __build_doctors_list(self):
         # get list of doctors that his patient is allowed to see
@@ -174,24 +165,41 @@ class AppointmentView(IViewable):
         self.cbo_doctors['values'] = self.doctors
         self.doctor_value.set(self.doctors[0])
 
-    def __build_schedule(self):
+    def __build_schedule(self, unavailable_slots):
+        print(unavailable_slots)
+        f = []
+
+        unavailable_slots_dict = {}
+
+        # create dictionary for constant time lookup O(1)
+        for u in unavailable_slots:
+            unavailable_slots_dict[u[0]] = True
+
         starting_row = 7
         self.schedules = {}
 
         hours = range(10, 15)
         minutes = range(0, 60, 15)
 
+        datestring = self.month_value.get() + " " + self.txt_day.get().zfill(2) + " " + self.txt_year.get()
+
         current_row = starting_row
         for hour in hours:
             for minute in minutes:
                 this_time = str(hour)+":"+str(minute).zfill(2)
+                time_timestamp = round(mktime(datetime.strptime(datestring + " " + this_time + ":00 +0400", "%B %d %Y %H:%M:%S %z").timetuple()))
+
                 self.schedules[this_time] = {}
                 self.schedules[this_time]['lbl'] = tk.Label(master=self.p_apmt, text=this_time, font=Style.font_regular)
                 self.schedules[this_time]['lbl'].grid(row=current_row, column=1)
 
-                # show button only if time is available
-                self.schedules[this_time]['btn'] = tk.Button(master=self.p_apmt, text="Schedule for: "+this_time, font=Style.font_regular, command=lambda t=this_time: self.__schedule_appointment(t))
-                self.schedules[this_time]['btn'].grid(row=current_row, column=2, sticky="we")
+                if unavailable_slots_dict.get(time_timestamp, False) is not True:
+                    # show button only if time is available
+                    self.schedules[this_time]['btn'] = tk.Button(master=self.p_apmt, text="Schedule for: "+this_time, font=Style.font_regular, command=lambda t=this_time: self.__schedule_appointment(t))
+                    self.schedules[this_time]['btn'].grid(row=current_row, column=2, sticky="we")
+                else:
+                    self.schedules[this_time]['lbl'] = tk.Label(master=self.p_apmt, text="Unavailable", font=Style.font_regular, bg="red")
+                    self.schedules[this_time]['lbl'].grid(row=current_row, column=2, sticky="we")
 
                 current_row += 1
 
@@ -209,7 +217,7 @@ class AppointmentView(IViewable):
             self.__build_doctors_list()
 
             # rebuild schedule without the removed timeslot
-            self.__build_schedule()
+            self.__build_schedule(self.search_available_bookings())
 
             # todo: rerender appointments list (not yet implemented)
 
